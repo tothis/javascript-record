09497ff110556a0d035d2fb8d87a0cac6c10c2f3
lilei <tothis@qq.com>
2020-01-19 09:57:33 +0800

    initial

--- base/js/require.js
+++ base/js/require.js
@@ -5,7 +5,7 @@ require.config({
     // 为文件或文件夹设置一个映射关系
     paths: {
         'jquery': 'https://cdn.staticfile.org/jquery/3.3.1/jquery',
-        'lib': '/js/lib', // 因为设置了baseUrl 所以这里的位置实际上是/static/js/lib
+        'lib': '/js/lib', // 因为设置了baseUrl所以这里的位置实际上是/static/js/lib
 
         // 为模块设置配置信息 在模块内可通过module.config()获取
         config: {
@@ -18,5 +18,5 @@ require.config({
 
 require(function(require, exports, module){
     // CommonJs形式的模块定义
-    var label = module.config().label; // -->moduleA
+    var label = module.config().label; // moduleA
 });
